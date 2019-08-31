81454328ad7dd752005376ee1e0d0636b9559b67
Diana <5275194+DianaNites@users.noreply.github.com>
2019-08-31 09:16:55 -0400

    Allow cargo workspaces to work.

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ struct Image {
 }
 
 /// Returns path to the bootloader binary.
-fn build_bootloader(meta: &Metadata, kernel_image: &Path) -> PathBuf {
+fn build_bootloader(meta: &Metadata, kernel_image: &Path, kernel_crate: &Package) -> PathBuf {
     let bootloader: &Package = meta
         .packages
         .iter()
@@ -46,8 +46,11 @@ fn build_bootloader(meta: &Metadata, kernel_image: &Path) -> PathBuf {
         .expect("Missing bootloader dependency");
     let bootloader_manifest = Path::new(&bootloader.manifest_path);
     let bootloader_target = bootloader_manifest.with_file_name("x86_64-bootloader.json");
-    let bootloader_triple = bootloader_target.file_stem().expect("Impossible");
+    let bootloader_triple = bootloader_target
+        .file_stem()
+        .expect("Couldn't parse bootloader target triple");
     let cargo = env::var_os("CARGO").expect("Missing CARGO environment variable.");
+
     // Build bootloader sysroot
     let exit = Command::new(&cargo)
         .arg("sysroot")
@@ -58,12 +61,13 @@ fn build_bootloader(meta: &Metadata, kernel_image: &Path) -> PathBuf {
         .status()
         .expect("Failed to build bootloader sysroot");
     assert!(exit.success(), "Failed to build bootloader sysroot");
+
     // Build bootloader
-    let mut exit = Command::new(&cargo);
-    exit.arg("build")
+    let mut cmd = Command::new(&cargo);
+    cmd.arg("build")
         // Required now, for some reason.
         .env("KERNEL", kernel_image)
-        .env("KERNEL_MANIFEST", meta.workspace_root.join("Cargo.toml"))
+        .env("KERNEL_MANIFEST", &kernel_crate.manifest_path)
         //
         .arg("--release")
         .arg("--target")
@@ -83,11 +87,11 @@ fn build_bootloader(meta: &Metadata, kernel_image: &Path) -> PathBuf {
     // Only include binary feature if it exists.
     // This allows supporting older `bootloader` versions.
     if bootloader.features.contains_key("binary") {
-        exit.arg("--features").arg("binary");
+        cmd.arg("--features").arg("binary");
     }
-    let exit = exit.status().expect("Failed to build bootloader");
-    //
+    let exit = cmd.status().expect("Failed to build bootloader");
     assert!(exit.success(), "Failed to build bootloader");
+
     bootloader_manifest
         .with_file_name("target")
         .join(bootloader_triple)
@@ -96,17 +100,8 @@ fn build_bootloader(meta: &Metadata, kernel_image: &Path) -> PathBuf {
 }
 
 /// Returns path to the kernel binary
-fn build_kernel(meta: &Metadata) -> PathBuf {
-    let self_bin = meta
-        .packages
-        .iter()
-        .find(|x| {
-            Path::new(&x.manifest_path)
-                .canonicalize()
-                .expect("Impossible")
-                == Path::new("Cargo.toml").canonicalize().expect("Impossible")
-        })
-        .expect("Couldn't find self in cargo-metadata")
+fn build_kernel(args: &Image, kernel_crate: &Package) -> PathBuf {
+    let self_bin = kernel_crate
         .targets
         .iter()
         .find(|x| x.kind.iter().find(|x| *x == "bin").is_some())
@@ -120,37 +115,44 @@ fn build_kernel(meta: &Metadata) -> PathBuf {
             .target
             .expect("Couldn't read target"),
     );
-    let target_triple = target.file_stem().expect("Impossible");
+    let target_triple = target.file_stem().expect("Couldn't parse target triple");
+
     // Build sysroot, just in case.
     let exit = Command::new(&cargo)
         .arg("sysroot")
         .status()
         .expect("Failed to build kernel sysroot");
     assert!(exit.success(), "Failed to build kernel sysroot");
+
     // Build kernel
-    let exit = Command::new(&cargo)
-        .arg("build")
-        .status()
-        .expect("Failed to build kernel");
+    let mut cmd = Command::new(&cargo);
+    cmd.arg("build");
+    if args.release {
+        cmd.arg("--release");
+    }
+    let exit = cmd.status().expect("Failed to build kernel");
     assert!(exit.success(), "Failed to build kernel");
-    //
-    // TODO: --release support
-    PathBuf::from(&meta.target_directory)
-        .join(target_triple)
-        .join("debug")
-        .join(&self_bin.name)
+
+    let mut final_path: PathBuf = PathBuf::from(&args.target_dir).join(target_triple);
+    if args.release {
+        final_path = final_path.join("release")
+    } else {
+        final_path = final_path.join("debug")
+    }
+    final_path.join(&self_bin.name)
 }
 
 /// Creates the final image by combining the bootloader and the kernel.
-fn create_image<T: AsRef<Path>, T2: AsRef<Path>>(kernel: T, bootloader: T2) {
-    let (kernel, bootloader) = (kernel.as_ref(), bootloader.as_ref());
+fn create_image(kernel: &Path, bootloader: &Path) {
     let kernel_bin = kernel.with_extension("bin");
     // Use llvm-objcopy. Bootloader requires this.
     let tools =
         LlvmTools::new().expect("Missing llvm tools from the llvm-tools-preview rustup component");
+
     let objcopy = tools
         .tool(&exe("llvm-objcopy"))
         .expect("Couldn't find llvm-objcopy");
+
     let _cmd = Command::new(objcopy)
         .arg("-I")
         .arg("elf64-x86-64")
@@ -167,6 +169,7 @@ fn create_image<T: AsRef<Path>, T2: AsRef<Path>>(kernel: T, bootloader: T2) {
         .write(true)
         .open(kernel_bin)
         .expect("Failed to open kernel.bin");
+
     // Padding
     let size = image
         .metadata()
@@ -174,6 +177,7 @@ fn create_image<T: AsRef<Path>, T2: AsRef<Path>>(kernel: T, bootloader: T2) {
         .len();
     let remain = size % BLOCK_SIZE;
     let padding = if remain > 0 { BLOCK_SIZE - remain } else { 0 };
+
     image
         .set_len((size + padding) as u64)
         .expect("Failed to pad kernel image");
@@ -189,10 +193,35 @@ fn main() {
         .exec()
         .expect("Unable to read Cargo.toml");
     //
+    let kernel_crate: &Package = {
+        if meta.workspace_members.len() == 1 {
+            &meta[&meta.workspace_members[0]]
+        } else {
+            let mut found = None;
+            let kernel_crate_name = args
+                .kernel_crate
+                .as_ref()
+                .expect("Must provide `--kernel-crate` when using virtual manifest");
+            for member in &meta.workspace_members {
+                let pkg = &meta[member];
+                if pkg.name == *kernel_crate_name {
+                    found = Some(pkg);
+                    break;
+                }
+            }
+            found.expect(&format!(
+                "Couldn't find kernel crate `{}`",
+                kernel_crate_name
+            ))
+        }
+    };
+
     println!("======Building kernel======");
-    let kernel = build_kernel(&meta);
+    let kernel = build_kernel(&args, kernel_crate);
+
     println!("====Building bootloader====");
-    let boot_out = build_bootloader(&meta, &kernel);
+    let boot_out = build_bootloader(&meta, &kernel, kernel_crate);
+
     // Combine Kernel and Bootloader
     println!("======Creating image======");
     create_image(&kernel, &boot_out);
